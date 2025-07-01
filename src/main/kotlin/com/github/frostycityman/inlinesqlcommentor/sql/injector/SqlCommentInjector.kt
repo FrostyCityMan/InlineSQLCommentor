146ac9f07b478d7014b63b654f63c12c8617e640
김도훈 <valentine9143@gmail.com>
2025-07-01 22:03:58 +0900

    제목: TableNameVisitor 지원 추가 및 SqlCommentInjector 수정
    - TableNameVisitor 인스턴스 추가 및 테이블 이름 처리 로직 반영
    - `injectComments` 메서드에서 테이블 이름 기반으로 주석 삽입하도록 수정
    - 코드 가독성 및 주석 추가

--- src/main/kotlin/com/github/frostycityman/inlinesqlcommentor/sql/injector/SqlCommentInjector.kt
+++ src/main/kotlin/com/github/frostycityman/inlinesqlcommentor/sql/injector/SqlCommentInjector.kt
@@ -1,7 +1,9 @@
 package com.github.frostycityman.inlinesqlcommentor.sql.injector
 
 import com.github.frostycityman.inlinesqlcommentor.sql.parser.ColumnCommentVisitor
+import com.github.frostycityman.inlinesqlcommentor.sql.parser.TableNameVisitor
 import com.github.frostycityman.inlinesqlcommentor.sql.provider.ColumnCommentProvider
+import com.tylerthrailkill.helpers.prettyprint.get
 
 /**
  * SQL 문자열 내 컬럼에 주석을 자동 삽입해주는 인젝터 클래스입니다.
@@ -22,7 +24,7 @@ import com.github.frostycityman.inlinesqlcommentor.sql.provider.ColumnCommentPro
  * @property commentProvider 컬럼 주석을 제공하는 주석 제공자 구현체
  */
 class SqlCommentInjector(
-    private val commentProvider: ColumnCommentProvider
+    private val commentProvider: ColumnCommentProvider,
 ) {
 
     /**
@@ -33,17 +35,24 @@ class SqlCommentInjector(
      * @return 컬럼 주석이 삽입된 SQL 문자열
      */
     fun injectComments(sql: String): String {
-        val visitor = ColumnCommentVisitor()
-
+        val commentVisitor = ColumnCommentVisitor()
+        val tableNameVisitor = TableNameVisitor()
         // SQL 내의 모든 컬럼명을 파싱하여 리스트로 추출
-        val columns = visitor.parseColumns(sql)
+        val columns = commentVisitor.parseColumns(sql)
+
+        val tableNames = mutableListOf<String>() // 결과를 담을 빈 리스트 생성
+
+        val tables = tableNameVisitor.parseTableNames(sql)
+        for (tableInfo in tables) {
+            tableNames.add(tableInfo.tableName) // 각 객체의 tableName을 리스트에 추가
+        }
 
         var commentedSql = sql
 
         // 각 컬럼에 대해 주석을 조회한 뒤 해당 위치에 주석 삽입
         columns.forEach { col ->
             // 주석 제공자에서 해당 컬럼의 주석을 조회
-            commentProvider.getComment("TEST_TABLE", col)?.let { comment ->
+            commentProvider.getComment(tableNames.get(0), col)?.let { comment ->
                 // SQL 문자열 내의 컬럼명을 주석 포함 문자열로 대체
                 commentedSql = commentedSql.replace(col, "$col /* $comment */")
             }
