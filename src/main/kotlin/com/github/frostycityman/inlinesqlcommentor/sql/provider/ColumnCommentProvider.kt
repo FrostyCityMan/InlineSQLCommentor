ed5fc10c5cea2342a8136bcd31ccce80740293c1
김도훈 <valentine9143@gmail.com>
2025-07-04 01:52:53 +0900

    제목: ColumnCommentProvider 메서드 추가 및 메서드명 수정
    - `getColumnComment` 메서드로 이름 변경: 명확성 향상
    - `getTableComment` 메서드 추가: 테이블 코멘트 조회 기능 지원
    - 불필요한 공백 줄 및 주석 정리

--- src/main/kotlin/com/github/frostycityman/inlinesqlcommentor/sql/provider/ColumnCommentProvider.kt
+++ src/main/kotlin/com/github/frostycityman/inlinesqlcommentor/sql/provider/ColumnCommentProvider.kt
@@ -15,7 +15,7 @@ interface ColumnCommentProvider {
      * @param columnName 컬럼 이름.
      * @return 컬럼 코멘트 문자열. 코멘트가 없거나, 테이블 또는 컬럼을 찾을 수 없는 경우 null을 반환합니다.
      */
-    fun getComment(tableName: String, columnName: String): String?
+    fun getColumnComment(tableName: String, columnName: String): String?
 
     /**
      * 현재 프로바이더가 사용하는 데이터 소스의 원시 연결 설정 정보를 반환합니다.
@@ -23,4 +23,7 @@ interface ColumnCommentProvider {
      * @return [RawConnectionConfig] 객체. 데이터 소스 정보를 사용할 수 없는 경우 null을 반환합니다.
      */
     fun getDataSourceConfig(): RawConnectionConfig?
+
+    fun getTableComment(tableName: String): String?
+
 }
